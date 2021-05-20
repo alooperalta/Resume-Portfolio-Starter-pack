d2ca1a9314bbe3ee2ea23b5cda4cf50fd39c6c2a
unknown <anuj.talwar.12@gmail.com>
2021-05-20 06:05:31 +0530

    First Commit

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
 import TypeWriter from "react-typewriter";
+import Typewriter from 'typewriter-effect';
 
 const Header = ({ data }) => {
   if (data) {
@@ -19,7 +20,13 @@ const Header = ({ data }) => {
   }
 
   return (
-    <header id="home">
+    <div className="wrapper">
+        <div className="page-header section-dark">
+            <div className="filter"></div>
+    		<div className="content-center">
+    			<div class="container">
+    				<div className="title-brand">
+            <header id="home">
       <nav id="nav-wrap">
         <a className="mobile-btn" href="#nav-wrap" title="Show navigation">
           Show navigation
@@ -65,7 +72,14 @@ const Header = ({ data }) => {
       <div className="row banner">
         <div className="banner-text">
           <h1 className="responsive-headline">
-            <TypeWriter typing={0.5}>{name ? `I'm ${name}.` : null}</TypeWriter>
+            {/* <TypeWriter typing={0.5}>{name ? `I'm ${name}.` : null}</TypeWriter> */}
+            <Typewriter
+              options={{
+                strings: [`I'm ${name}.`, "I'm a #PAPAFan."],
+                autoStart: true,
+                loop: true,
+              }}
+            />
           </h1>
           <h3>
             Based in {city}. <span>{occupation}</span>. {description}.
@@ -81,6 +95,21 @@ const Header = ({ data }) => {
         </a>
       </p>
     </header>
+    					<div className="fog-low">
+    						<img src="http://demos.creative-tim.com/paper-kit-2/assets/img/fog-low.png" alt="" />
+    					</div>
+    					<div className="fog-low right">
+    						<img src="http://demos.creative-tim.com/paper-kit-2/assets/img/fog-low.png" alt="" />
+    					</div>
+    				</div>
+    			</div>
+    		</div>
+            <div className="moving-clouds">
+
+            </div>
+    	</div>
+    </div>
+    
   );
 };
 
